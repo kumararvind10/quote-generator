5687d2e6a89821dd3ee02c74195082a50cb145ec
kumararvind10 <31480328+kumararvind10@users.noreply.github.com>
2023-07-09 00:45:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function removeLoadingSpinner() {
 async function getQoute() {
     showloadingSpinner();
     try{
-        let response = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json', { mode: 'no-cors' });
+        let response = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json');
         console.log("response", response)
         let data = await response.json();
         console.log('data',data)
